46a98790ec2cee7fb564e0dc74bedb6d5eebeb73
vplasencia <v.pcalana@gmail.com>
2022-08-20 23:08:59 -0400

    Change header button style

--- lib/components/Nav.jsx
+++ lib/components/Nav.jsx
@@ -12,45 +12,42 @@ export const Nav = (props) => {
 
   const developersPage = router.pathname.match('developers')
 
-  const navLinkClasses = 'capitalize text-center leading-none rounded-full flex justify-start items-center text-lg py-3 px-4 lg:px-8 trans tracking-wider outline-none focus:outline-none active:outline-none text-white'
+  const navLinkClasses =
+    'capitalize text-center leading-none rounded-full flex justify-start items-center text-lg py-3 px-4 lg:px-8 trans tracking-wider outline-none focus:outline-none active:outline-none text-white'
 
-  return <>
-    <nav
-      className='justify-end items-center hidden sm:flex w-2/3'
-    >
-      <Link
-        href='/developers'
-        as='/developers'
-        shallow
-      >
-        <a
-          className={classnames(
-            'mr-3',
-            navLinkClasses,
-            {
+  return (
+    <>
+      <nav className='justify-end items-center hidden sm:flex w-2/3'>
+        <Link href='/developers' as='/developers' shallow>
+          <a
+            className={classnames('mr-3', navLinkClasses, {
               'text-white hover:text-highlight-2': !developersPage,
               'text-highlight-2 hover:text-highlight-2': developersPage
-            }
-          )}
-        >
-          {t('developers')}
-        </a>
-      </Link>
+            })}
+          >
+            {t('developers')}
+          </a>
+        </Link>
 
-      <Link
-        href='https://app.pooltogether.com'
-        as='https://app.pooltogether.com'
-      >
-        <a
+        <Link href='https://app.pooltogether.com' as='https://app.pooltogether.com'>
+          {/* <a
           className={classnames(
             'inline-flex items-center justify-center uppercase font-bold tracking-wider outline-none focus:outline-none active:outline-none',
             'hover:bg-default rounded-full border-2 border-highlight-2 px-10 py-1 trans trans-fast text-lg',
           )}
         >
           {t('app')}
-        </a>
-      </Link>
-    </nav>
-  </>
-    
+        </a> */}
+
+          <a
+            id='poolBtn'
+            href='https://lossless-ui.vercel.app/'
+            className='rounded-lg border-2 text-green border-green bg-default px-5 py-1 text-lg font-semibold hover:text-purple hover:bg-green transition-colors duration-300 ease-in-out'
+          >
+            App
+          </a>
+        </Link>
+      </nav>
+    </>
+  )
 }
