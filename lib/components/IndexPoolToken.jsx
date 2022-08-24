16a87bd5c54285817e9a8b5947466f2e1ec4dfc3
vplasencia <v.pcalana@gmail.com>
2022-08-24 18:46:16 -0400

    Change Pool Token section style

--- lib/components/IndexPoolToken.jsx
+++ lib/components/IndexPoolToken.jsx
@@ -45,13 +45,10 @@ export const IndexPoolToken = (props) => {
                 Deposit to receive POOL tokens
               </ButtonLink>
             </div> */}
-            <div className='grid'>
-
-            </div>
             <a
               id='poolBtn'
               href='https://lossless-ui.vercel.app/'
-              className='w-full rounded-lg border-2 text-slate900 border-green bg-green px-5 py-2 text-xl font-semibold hover:text-green hover:bg-darkened transition-colors duration-300 ease-in-out'
+              className='w-full rounded-lg border-2 text-slate900 border-blue-2 bg-blue-2 px-5 py-2 text-xl font-semibold hover:text-blue-2 hover:bg-darkened transition-colors duration-300 ease-in-out'
             >
               Deposit to receive POOL tokens
             </a>
@@ -66,13 +63,31 @@ export const IndexPoolToken = (props) => {
                 Learn about governance
               </ButtonLink>
             </div> */}
-            <div className='mt-10'>
+            <div className='flex justify-center items-center mt-10'>
               <a
                 href='https://github.com/HarmonyZKDAO/lossless-website'
-                rel="noreferrer noopener nofollow"
-                className='text-green hover:underline hover:text-green'
+                rel='noreferrer noopener nofollow'
+                className='text-blue-2 hover:underline hover:text-blue-2 w-max-content'
               >
-                Learn about governance
+                <div className='flex space-x-1'>
+                  <span>Learn about governance</span>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    width='24'
+                    height='24'
+                    viewBox='0 0 24 24'
+                    fill='none'
+                    stroke='currentColor'
+                    strokeWidth='2'
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    className='feather feather-external-link'
+                  >
+                    <path d='M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6'></path>
+                    <polyline points='15 3 21 3 21 9'></polyline>
+                    <line x1='10' y1='14' x2='21' y2='3'></line>
+                  </svg>
+                </div>
               </a>
             </div>
           </div>
