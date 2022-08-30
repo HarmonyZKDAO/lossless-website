a60e61aae04fc03db56327867d4b61fcfa349d00
vplasencia <v.pcalana@gmail.com>
2022-08-30 16:46:55 -0400

    Change share social media image and meta tags

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -13,11 +13,11 @@ class MyDocument extends Document {
   }
 
   render() {
-    const title = 'PoolTogether'
-    const url = `https://pooltogether.com`
+    const title = 'Lossless'
+    const url = `https://lossless-website.vercel.app`
     const description = `Win $ every week just by saving your money.`
     const keywords = 'ethereum'
-    const twitterHandle = '@PoolTogether_'
+    const twitterHandle = ''
 
     return (
       <Html>
@@ -26,17 +26,17 @@ class MyDocument extends Document {
           <>
             <link rel='icon' href='/favicon.png' type='image/x-icon' />
 
-            <meta name='theme-color' content='#1e0b43' />
+            <meta name='theme-color' content='#4f46e5' />
             <meta name='description' content={description} />
             <meta name='keywords' content={keywords} />
-            <meta name='author' content='PoolTogether' />
+            <meta name='author' content='Lossless' />
 
             <meta property='og:title' content={title} />
             <meta property='og:description' content={description} />
             <meta property='og:site_name' content={title} />
             <meta property='og:url' content={url} />
             <meta property='og:type' content='website' />
-            <meta property='og:image' content={`${url}/pooltogether-facebook-share-image-1200-630@2x.png`} />
+            <meta property='og:image' content={`${url}/socialMedia.png`} />
             <meta property='og:rich_attachment' content='true' />
             <meta property='og:image:width' content='1200' />
             <meta property='og:image:height' content='630' />
@@ -45,7 +45,7 @@ class MyDocument extends Document {
             <meta property='twitter:description' content={description} />
             <meta property='twitter:card' content='summary_large_image' />
             <meta property='twitter:site' content={twitterHandle} />
-            <meta property='twitter:image:src' content={`${url}/pooltogether-twitter-share-image-1200-675@2x.png`} />
+            <meta property='twitter:image:src' content={`${url}/socialMedia.png`} />
             <meta property='twitter:url' content={url} />
             <meta property='twitter:creator' content={twitterHandle} />
           </>
